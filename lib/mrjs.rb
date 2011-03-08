fc8ae93e0ddc9e985d066f585e237f3e3df6f9a8
Damian Nurzynski <dnurzynski@gmail.com>
2011-03-08 13:44:49 +0100

    add requires

--- lib/mrjs.rb
+++ lib/mrjs.rb
@@ -1,5 +1,17 @@
 require 'rubygems'
 require 'harmony'
+$: << ":#{File.dirname(__FILE__)}"
+
+require 'mrjs/runner'
+require 'mrjs/config'
+require 'mrjs/drivers/base'
+require 'mrjs/drivers/harmony'
+require 'mrjs/formatters/base'
+require 'mrjs/adapters'
+require 'mrjs/adapters/qunit'
+require 'mrjs/group_result'
+require 'mrjs/example_result'
+require 'mrjs/assertion_result'
 
 module Mrjs
 
